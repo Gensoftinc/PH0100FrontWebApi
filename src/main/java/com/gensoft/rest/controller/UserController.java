96ba854c171e1d9f1479094d458460b0d8a11c29
Jarves <Jarves@DESKTOP-TLMVIJ2>
2018-07-29 18:21:18 +0530

    //kaushik

--- src/main/java/com/gensoft/rest/controller/UserController.java
+++ src/main/java/com/gensoft/rest/controller/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gensoft.database.model.User;
+import com.gensoft.common.model.User;
 import com.gensoft.repository.UserRepository;
 import com.gensoft.rest.constant.APIName;
 
