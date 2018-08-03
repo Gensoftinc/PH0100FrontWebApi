f35a2fbad6b654ee0e7fbed02d6ad1952acb25d9
agolechha <agolechha@gensoft.com>
2018-08-03 12:31:42 -0700

    PH0100:
    Modified class to improve code.

--- src/main/java/com/gensoft/frontend/products/service/CartService.java
+++ src/main/java/com/gensoft/frontend/products/service/CartService.java
@@ -2,9 +2,25 @@ package com.gensoft.frontend.products.service;
 
 import com.gensoft.common.model.Cart;
 
+/**
+ * Cart service
+ * <p>
+ * @author Amruta, (c) Copyright 2018 GenSoft, Inc. All Rights Reserved.
+ */
 public interface CartService {
 	
+	/**
+	 * This will insert the product into the cart.
+	 * <p>
+	 * @param cart - cart object which will contain all the detail and going to be saved in database.
+	 * @return {@link Cart} - object.
+	 */
 	public Cart insertProductsToCart(Cart cart);
 
+	/**
+	 * TODO insert comment here
+	 * @param cartId
+	 * @return
+	 */
     public int deleteProductFromCart(int cartId);
 }
--- src/main/java/com/gensoft/frontend/products/service/GetCartProductsServiceImp.java
+++ src/main/java/com/gensoft/frontend/products/service/GetCartProductsServiceImp.java
@@ -8,32 +8,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.gensoft.common.model.Cart;
 import com.gensoft.common.model.GetCartProducts;
-import com.gensoft.common.repository.CartRepository;
 import com.gensoft.common.repository.GetCartProductsRepository;
 import com.gensoft.exception.ApplicationException;
 
+/**
+ * TODO Insert comment here.
+ * <p>
+ * 
+ * @author Amruta, (c) Copyright 2018 GenSoft, Inc. All Rights Reserved.
+ */
 @Service
-public class GetCartProductsServiceImp implements GetCartProductsService{
-	
+public class GetCartProductsServiceImp implements GetCartProductsService {
+
 	/** LOGGER */
 	private static final Logger LOGGER = LoggerFactory.getLogger(GetCartProductsServiceImp.class);
-	
+
 	@Autowired
 	private GetCartProductsRepository getCartProductsRepository;
-	
-	
+
 	@Override
-	public List<GetCartProducts> getCartProductsOfUser(int  userId) {
-		List<GetCartProducts> getCartProductsList = new ArrayList<GetCartProducts>();
+	public List<GetCartProducts> getCartProductsOfUser(int userId) {
+		List<GetCartProducts> getCartProductsList = new ArrayList<>();
 		try {
 			getCartProductsList = getCartProductsRepository.getCartProductsBYUserId(userId);
 		} catch (Exception e) {
-			LOGGER.error("Error while inserting the product category.", e);
-			throw new ApplicationException("Error while inserting the product category.", e);
+			LOGGER.error("Error while getting the cart product of user.", e);
+			throw new ApplicationException("Error while getting the cart product of user.", e);
 		}
 		return getCartProductsList;
 	}
-
 }
